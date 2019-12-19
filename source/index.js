efdeb78277d403454cc11b7ded8a3f75b5688225
Joe Hand <joe@datproject.org>
2019-12-19 09:06:29 -0800

    Redirect to new dat.foundation site (retire)

--- source/index.js
+++ source/index.js
@@ -4,6 +4,9 @@ require('./design')
 var choo = require('choo')
 
 var app = choo()
+app.use(() => {
+  if (window.location.host === 'dat.land') return window.location = 'https://dat.foundation'
+})
 app.use(require('enoki/choo')())
 app.use(require('./plugins/scroll'))
 app.route('*', require('./views/wrapper'))
